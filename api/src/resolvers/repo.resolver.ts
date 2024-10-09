f763242fa2042d07e965f919be1491234f981eda
scyanth <34488277+scyanth@users.noreply.github.com>
2024-10-09 10:30:26 +0200

    ajout mutation simple

--- api/src/resolvers/repo.resolver.ts
+++ api/src/resolvers/repo.resolver.ts
@@ -1,13 +1,43 @@
 import { Repo } from "../entities/repo";
-import { Query, Resolver } from "type-graphql";
+import { Arg, Field, InputType, Mutation, Query, Resolver } from "type-graphql";
+
+@InputType()
+class createRepoInput {
+  @Field()
+  id: string;
+
+  @Field()
+  name: string;
+
+  @Field()
+  url: string;
+
+  @Field()
+  isFavorite: boolean;
+
+}
 
 @Resolver(Repo)
 export default class RepoResolver {
   @Query(() => [Repo])
-  async getReposFull() {
+  async getRepos() {
     const repos = await Repo.find();
-    console.info(repos);
+    console.log(repos);
     return repos;
+  }
+
+  @Mutation(() => Repo)
+  async createRepo(@Arg("repoInput") repoInput: createRepoInput) {
+    console.log(repoInput);
+    //const newRepo = await Repo.create(repoInput);
+    const newRepo = new Repo();
+
+    newRepo.id = repoInput.id;
+    newRepo.name = repoInput.name;
+    newRepo.url = repoInput.url;
+    newRepo.isFavorite = repoInput.isFavorite;
+
+    return newRepo;
   }
 
 }
