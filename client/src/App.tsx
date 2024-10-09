eb3be688d21489f0f56bf7f10877fba59b64a2a0
scyanth <34488277+scyanth@users.noreply.github.com>
2024-10-09 12:11:10 +0200

    adapt react pour graphql

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,71 +1,83 @@
 import './App.css';
-import { useEffect, useState } from "react";
-import connexion from "./services/connexion";
+// import { useEffect, useState } from "react";
+// import connexion from "./services/connexion";
 import { useQuery, gql  } from "@apollo/client";
 import RepoCard from "./components/RepoCard";
 import { Repo } from "./types/RepoType";
-import { Lang } from "./types/LangType";
+// import { Lang } from "./types/LangType";
+
+const getRepos = gql`
+  query GetRepos {
+    getRepos {
+      id
+      isFavorite
+      name
+      url
+    }
+  }
+`;
 
 function App() {
 
-  const [repos, setRepos] = useState<Repo[]>([]);
-  const [filteredRepos, setFilteredRepos] = useState<Repo[]>([]);
-  const [langs, setLanguages] = useState<Lang[]>([]);
+  const { loading, error, data, refetch } = useQuery(getRepos);
 
-  useEffect(() => {
-    console.log("I'm the useEffect");
-    const fetchRepos = async () => {
-      try {
-        // const repos = await connexion.get<Repo[]>("/api/repos");
-        const repos = await useQuery(gql`
-          query getRepos {
-            repos {
-              id, name, url, languages, isFavorite
-            }
-          }
-        `)
-        // const langs = await connexion.get<Lang[]>("/api/langs");
-        const langs = await useQuery(gql`
-          query getLangs {
-            langs {}
-          }
-        `)
-        setRepos(repos.data);
-        setFilteredRepos(repos.data);
-        setLanguages(langs.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchRepos();
-  }, []);
+  if (loading) return <h1>Loading...</h1>
 
-  const reposFilter = (lang_label : string) => {
-    const filteredRepos = repos.filter((repo) => 
-      repo.languages.some(lang => 
-        (lang.label === lang_label))
-    );
-    setFilteredRepos(filteredRepos);
-  };
+  // const [repos, setRepos] = useState<Repo[]>([]);
+  // const [filteredRepos, setFilteredRepos] = useState<Repo[]>([]);
+  // const [langs, setLanguages] = useState<Lang[]>([]);
 
-  const reposReset = () => {
-    setFilteredRepos(repos);
-  };
+  // useEffect(() => {
+  //   console.log("I'm the useEffect");
+  //   const fetchRepos = async () => {
+  //     try {
+  //       // const repos = await connexion.get<Repo[]>("/api/repos");
+  //       const repos = await useQuery(gql`
+  //         query getRepos {
+  //           repos {
+  //             id, name, url, languages, isFavorite
+  //           }
+  //         }
+  //       `)
+  //       // const langs = await connexion.get<Lang[]>("/api/langs");
+  //       const langs = await useQuery(gql`
+  //         query getLangs {
+  //           langs {}
+  //         }
+  //       `)
+  //       setRepos(repos.data);
+  //       setFilteredRepos(repos.data);
+  //       setLanguages(langs.data);
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   };
+  //   fetchRepos();
+  // }, []);
+
+  // const reposFilter = (lang_label : string) => {
+  //   const filteredRepos = repos.filter((repo) => 
+  //     repo.languages.some(lang => 
+  //       (lang.label === lang_label))
+  //   );
+  //   setFilteredRepos(filteredRepos);
+  // };
+
+  // const reposReset = () => {
+  //   setFilteredRepos(repos);
+  // };
 
   return (
     <main>
       <header>
         <h2>Mes repos Github</h2>
-        <button onClick={reposReset}> Tous les langages </button>
-        {langs.map((lang: Lang) => (
-          <button onClick={() => reposFilter(lang.label)}> {lang.label} </button>
-        ))}
       </header>
       <div className='reposContainer'>
-        {filteredRepos.map((repo: Repo) => (
+        {data.getRepos.map((repo: Repo) => (
           <RepoCard name={repo.name} url={repo.url} languages={repo.languages} id={repo.id} isFavorite={repo.isFavorite} />
         ))}
       </div>
+      <button onClick={refetch}></button>
     </main>
   )
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import connexion from "./services/connexion";
-import { ApolloProvider, useQuery, gql  } from "@apollo/client";
-import Detail from './pages/Detail'
+import { ApolloProvider, /* useQuery, gql */  } from "@apollo/client";
+// import Detail from './pages/Detail'
 import App from './App.tsx'
 import './index.css'
 
@@ -12,22 +12,22 @@ const router = createBrowserRouter([
     path: "/",
     element: <App />,
   },
-  {
-    path: "/detail/:id",
-    element: <Detail />,
-    loader: async ({ params }) => {
-      // const repos = await connexion.get(`/api/repos/${params.id}`);
-      const repos = await useQuery(gql`
-          query getRepos {
-            repos {
-              id: ${params.id}
-            }
-          }
-        `)
-      console.log("Loader", repos);
-      return repos.data;
-    },
-  }
+  // {
+  //   path: "/detail/:id",
+  //   element: <Detail />,
+  //   loader: async ({ params }) => {
+  //     // const repos = await connexion.get(`/api/repos/${params.id}`);
+  //     const repos = await useQuery(gql`
+  //         query getRepos {
+  //           repos {
+  //             id: ${params.id}
+  //           }
+  //         }
+  //       `)
+  //     console.log("Loader", repos);
+  //     return repos.data;
+  //   },
+  // }
 ]);
 
 createRoot(document.getElementById('root')!).render(
